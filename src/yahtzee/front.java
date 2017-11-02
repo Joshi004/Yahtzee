b01beb98b5524ad6960ea2eec131f56ace66c0ba
Naresh Joshi <thinkjoshi@gmail.com>
2017-11-02 14:09:05 +0530

    Create outline of project
    Create the table and lables

--- src/yahtzee/front.java
+++ src/yahtzee/front.java
@@ -29,10 +29,10 @@ this.setBackground(Color.green);
 
 String playerName[]=userInfo();
 int diceNum[] = new int[5]; 
-GLabel score[][]=new GLabel[17][playerName.length+1];
+GLabel score[][]=new GLabel[18][playerName.length+1];
+GRect cover[][]=new GRect[18][playerName.length+1];
 
-
-setup(playerName,score);
+setup(playerName,score,cover);
 /*
 rollDice();
 selectDice();
@@ -43,7 +43,7 @@ assignScore();
 	
 }// Main ends 	
 
-private int setup(String playerName[],GLabel score[][])
+private int setup(String playerName[],GLabel score[][],GRect cover[][])
 {
 diceRaw[0]=new GImage("file:///E:/work_space/JAVA/yahtzee/images/0.png");
 diceRaw[1]=new GImage("file:///E:/work_space/JAVA/yahtzee/images/1.png");
@@ -52,39 +52,138 @@ diceRaw[3]=new GImage("file:///E:/work_space/JAVA/yahtzee/images/3.png");
 diceRaw[4]=new GImage("file:///E:/work_space/JAVA/yahtzee/images/4.png");
 diceRaw[5]=new GImage("file:///E:/work_space/JAVA/yahtzee/images/5.png");
 diceRaw[6]=new GImage("file:///E:/work_space/JAVA/yahtzee/images/6.png");
+
+double coverW=windowW/5;
+
 double y=0,x=0;
 for(int i=0;i<5;i++)		
 {	
-	dice[i]=new GImage("file:///E:/work_space/JAVA/yahtzee/images/0.png");	
+	dice[i]=new GImage("file:///E:/work_space/JAVA/yahtzee/images/3.png");	
 	dice[i].setSize(windowW/20,windowW/20);
-	y=y+2*dice[0].getHeight()/1.5;
+	y=y+2*dice[0].getHeight()/1.25;
 	x=dice[0].getWidth();
 	add(dice[i],x,y);
+	
 }
-x=windowW/3;
+x=windowW/4;
 y=2*dice[0].getHeight();
 
-for(int i=0;i<17;i++)
+
+// Making First Column
+for(int i=0;i<18;i++)
 {
-for(int j=0;j<playerName.length+1;j++)
-{
+	int j=0;
 	
-	
-	score[i][j]=new GLabel("hello gamer bhai",x,y);
+	score[i][j]=new GLabel("Large",x-30,y);
 	score[i][j].setColor(Color.BLUE);
-	score[i][j].sendToFront();
+	//score[i][j].sendToFront();
 	score[i][j].setFont("Serif-20");
 	add(score[i][j]);
 	
 	
-	GRect cover = new GRect(x,y-score[i][j].getHeight(),score[i][j].getWidth(),score[i][j].getHeight());
-	cover.sendBackward();
-	add(cover);
+	
+	cover[i][j] = new GRect(score[i][j].getWidth(),score[i][j].getHeight());
+	cover[i][j].setSize(coverW,score[i][j].getHeight());
+	double subs =(cover[i][j].getWidth()-score[i][j].getWidth())/2;
+	//cover[i][j].sendBackward();
+	add(cover[i][j],x-subs,y-score[i][j].getHeight()+score[i][j].getDescent());
 	x=x+score[i][j].getWidth()+10;
-}
+
+
 y=y+score[0][0].getHeight();
-x=windowW/3;
+x=windowW/4;
 }
+
+// making First Row
+
+x=windowW/5+cover[0][0].getWidth();
+y=2*dice[0].getHeight();
+
+for(int j=1;j<playerName.length+1;j++)
+{
+int i=0;
+
+x=cover[i][j-1].getX()+cover[i][j-1].getWidth();
+double width=3*coverW/playerName.length;
+
+score[i][j]=new GLabel("Naresh is me",x,y);
+score[i][j].setColor(Color.BLUE);
+score[i][j].sendToFront();
+score[i][j].setFont("Serif-20");
+add(score[i][j],x+width/2-score[i][j].getWidth()/2,y);
+
+
+cover[i][j] = new GRect(width,score[i][j].getHeight());
+double subs =(cover[i][j].getWidth()-score[i][j].getWidth())/2;
+cover[i][j].sendBackward();
+add(cover[i][j],x,y-score[i][j].getHeight()+score[i][j].getDescent());
+
+}
+
+// Adding Score Values
+for(int i=1;i<18;i++)
+{
+for(int j=1;j<playerName.length+1;j++)
+{
+	x=cover[i][j-1].getX()+cover[i][j-1].getWidth(); // To start from the end of previous rect
+	y=score[i][j-1].getY();
+	double width=3*coverW/playerName.length;		//Window/6 is default lnegth choosen for first column and score will extend only to 3 times of it and will be decided by the number of players
+
+	score[i][j]=new GLabel("10",x,y);
+	score[i][j].setColor(Color.BLUE);
+	score[i][j].sendToFront();
+	score[i][j].setFont("Serif-20");
+	add(score[i][j],x+width/2-score[i][j].getWidth()/2,y);
+
+	y=cover[i][j-1].getY();
+	cover[i][j] = new GRect(width,score[i][j].getHeight());
+	double subs =(cover[i][j].getWidth()-score[i][j].getWidth())/2;
+	cover[i][j].sendBackward();
+	add(cover[i][j],x,y);
+	
+}
+}
+
+// Assigning names in table
+for(int j=1;j<playerName.length+1;j++)
+{
+int i=0;
+score[i][j].setLabel(playerName[j-1].toUpperCase());
+score[i][j].setLocation(cover[i][j].getX()+(cover[i][j].getWidth()-score[i][j].getWidth())/2,score[i][j].getY());
+}
+
+// Assigning text to column 1 labels
+score[0][0].setLabel("CATEGORIES");
+score[0][0].setLocation(score[0][0].getX()-40, score[0][0].getY());
+		
+score[1][0].setLabel("Ones");
+score[2][0].setLabel("Twos");
+score[3][0].setLabel("Threes");
+score[4][0].setLabel("Fours");
+score[5][0].setLabel("Fives");
+score[6][0].setLabel("Sixes");
+
+score[7][0].setLabel("UPPER SCORE");
+score[7][0].setLocation(score[7][0].getX()-40, score[7][0].getY());
+score[8][0].setLabel("UPPER BONUS (35)");
+score[8][0].setLocation(score[8][0].getX()-40, score[8][0].getY());
+
+score[9][0].setLabel("Three Of A Kind");
+score[10][0].setLabel("Four Of A Kind");
+score[11][0].setLabel("Full House (25)");
+score[12][0].setLabel("Small Straight (30)");
+score[13][0].setLabel("Large Straight (40)");
+score[14][0].setLabel("Yahtzee ! (50)");
+score[15][0].setLabel("Chance");
+
+score[16][0].setLabel("LOWER SCORE");
+score[16][0].setLocation(score[16][0].getX()-40, score[16][0].getY());
+score[17][0].setLabel("TOTAL SCORE");
+score[17][0].setLocation(score[17][0].getX()-40, score[17][0].getY());
+
+
+
+
 
 return 1;
 }
